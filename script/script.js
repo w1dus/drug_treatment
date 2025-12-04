601957b26cac857aadb2097219c5479c86ac00ba
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-12-04 17:04:12 +0900

    마약치료

--- script/script.js
+++ script/script.js
@@ -1,7 +1,60 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+  slide_menu()
+  card_news_slide();
 })
 
+const card_news_slide = () => {
+  var swiper = new Swiper(".main .notice-section .main-news .mySwiper", {
+    loop: true,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".main .notice-section .main-news .swiper-pagination",
+    },
 
+    on: {
+      // 초기 로드 시
+      init: function () {
+        updateSlideNumber(this);
+      },
+      // 슬라이드 변경 시
+      slideChange: function () {
+        updateSlideNumber(this);
+      }
+    }
+  });
+
+  // 숫자 업데이트 함수
+  function updateSlideNumber(swiper) {
+    const current = swiper.realIndex + 1;
+    const total = swiper.slides.length - swiper.loopedSlides * 2;
+
+    const slideNumber = document.querySelector(".main .notice-section .main-news .slide-number");
+    if (slideNumber) {
+      slideNumber.textContent = `${current} / ${total}`;
+    }
+  }
+};
+
+
+const slide_menu = () => {
+  $('header .real .menu-btn').click(function(){
+    $('.slide-menu-close').addClass('show');
+    $('.slide-menu').addClass('show');
+  })
+
+  $('.slide-menu .btn-wrap .close-btn').click(function(){
+    $('.slide-menu-close').removeClass('show');
+    $('.slide-menu').removeClass('show');
+  })
+
+  $('.slide-menu-close').click(function(){
+    $('.slide-menu-close').removeClass('show');
+    $('.slide-menu').removeClass('show');
+  })
+}
